ff5d72ca415706e12bba4c67c4126fff7c662db2
Josh <josh@blobfolio.com>
2016-09-05 08:09:14 -0700

    version bump

--- blob-common/index.php
+++ blob-common/index.php
@@ -3,7 +3,7 @@
 Plugin Name: Tutan Common
 Plugin URI: https://blobfolio.com/repo/blob-common/
 Description: Functions to assist common theme operations.
-Version: 1.1.5
+Version: 1.1.6
 Author: Blobfolio, LLC
 Author URI: https://blobfolio.com/
 License: GPLv2 or later
