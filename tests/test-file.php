25b1ac9276c8d1d251e2bbe16bf219dc3e92d6d9
Josh Stoik <josh@blobfolio.com>
2018-04-22 14:49:02 -0700

    unit tests for ::copy, ::mkdir, ::hash_dir, ::scan_dir

--- tests/test-file.php
+++ tests/test-file.php
@@ -25,6 +25,31 @@ class file_tests extends \PHPUnit\Framework\TestCase {
 	// --------------------------------------------------------------------
 
 	/**
+	 * ::copy()
+	 *
+	 * @return void Nothing.
+	 */
+	function test_copy() {
+		$from = self::ASSETS;
+		$base = dirname(self::ASSETS) . '/copied/';
+		$to = dirname(self::ASSETS) . '/copied/assets/';
+
+		if (@file_exists($base)) {
+			file::rmdir($base);
+			if (@file_exists($base)) {
+				$this->markTestSkipped('The test directory already exists.');
+			}
+		}
+
+		file::copy($from, $to);
+		$this->assertEquals(true, @is_dir($to));
+		$this->assertEquals(false, file::empty_dir($to));
+
+		file::rmdir($base);
+		$this->assertEquals(false, @is_dir($base));
+	}
+
+	/**
 	 * ::data_uri()
 	 *
 	 * @return void Nothing.
@@ -54,6 +79,45 @@ class file_tests extends \PHPUnit\Framework\TestCase {
 	}
 
 	/**
+	 * ::dir_hash
+	 *
+	 * @return void Nothing.
+	 */
+	function test_hash_dir() {
+		$path = dirname(__FILE__);
+
+		// Try something made up.
+		$this->assertSame(false, file::hash_dir($path, 'foobar'));
+
+		// Try MD5.
+		$hash = file::hash_dir($path);
+		$this->assertSame(32, strlen($hash));
+
+		// Add a file.
+		$file = $path . '/hashdir.file';
+		@file_put_contents($file, 'Hello World');
+		$hash2 = file::hash_dir($path);
+		$this->assertSame(32, strlen($hash2));
+		$this->assertSame(false, ($hash === $hash2));
+
+		// Remove the file.
+		@unlink($file);
+		$hash3 = file::hash_dir($path);
+		$this->assertSame(32, strlen($hash3));
+		$this->assertSame(true, ($hash === $hash3));
+
+		// Try an alternative file hash.
+		$hash4 = file::hash_dir($path, 'md5', 'crc32b');
+		$this->assertSame(32, strlen($hash4));
+		$this->assertSame(false, ($hash === $hash4));
+
+		// Try an alternative dir hash.
+		$hash5 = file::hash_dir($path, 'crc32b', 'md5');
+		$this->assertSame(8, strlen($hash5));
+		$this->assertSame(false, ($hash4 === $hash5));
+	}
+
+	/**
 	 * ::leadingslash()
 	 *
 	 * @dataProvider data_leadingslash
@@ -63,6 +127,36 @@ class file_tests extends \PHPUnit\Framework\TestCase {
 	 */
 	function test_leadingslash($path, $expected) {
 		$this->assertEquals($expected, file::leadingslash($path));
+	}
+
+	/**
+	 * ::mkdir() and ::rmdir()
+	 */
+	function test_mkdir_rmdir() {
+		$base = static::ASSETS . 'rmdir-test/';
+		$path = static::ASSETS . 'rmdir-test/subdir/';
+		$file = $path . 'test.txt';
+
+		if (@file_exists($base)) {
+			file::rmdir($base);
+			if (@file_exists($base)) {
+				$this->markTestSkipped('The test directory already exists.');
+			}
+		}
+
+		// Make the directory.
+		file::mkdir($path, 0755);
+		$this->assertSame(true, is_dir($path));
+		clearstatcache();
+		$this->assertSame(decoct(0755), decoct(@fileperms($path) & 0777));
+
+		// Add a file.
+		@file_put_contents($file, 'Hello World');
+		$this->assertSame(true, @file_exists($file));
+
+		// Remove it the directories and file.
+		file::rmdir($base);
+		$this->assertSame(false, @is_dir($base));
 	}
 
 	/**
@@ -79,29 +173,30 @@ class file_tests extends \PHPUnit\Framework\TestCase {
 	}
 
 	/**
-	 * ::rmdir()
+	 * ::scandir()
+	 *
+	 * @return void Nothing.
 	 */
-	function test_rmdir() {
-		$path = static::ASSETS . 'rmdir-test/subdir/';
+	function test_scandir() {
+		$path = dirname(__FILE__);
 
-		// Make a directory.
-		if (!@file_exists($path)) {
-			@mkdir($path, 0755, true);
-		}
-		if (!@file_exists($path)) {
-			$this->markTestSkipped('The test directory could not be created.');
-		}
+		// Files and directories.
+		$files = file::scandir($path);
+		$this->assertSame(true, in_array("{$path}/assets", $files, true));
+		$this->assertSame(true, in_array("{$path}/assets/pi.svg", $files, true));
+		$this->assertSame(true, in_array("{$path}/test-file.php", $files, true));
 
-		// Toss a file in there.
-		@file_put_contents("{$path}test.file", 'hello');
-		if (!@file_exists("{$path}test.file")) {
-			$this->markTestSkipped('The test file could not be created.');
-		}
+		// Only files.
+		$files = file::scandir($path, true, false);
+		$this->assertSame(false, in_array("{$path}/assets", $files, true));
+		$this->assertSame(true, in_array("{$path}/assets/pi.svg", $files, true));
+		$this->assertSame(true, in_array("{$path}/test-file.php", $files, true));
 
-		// Now delete it.
-		$path = dirname($path);
-		$this->assertSame(true, file::rmdir($path));
-		$this->assertSame(false, @file_exists($path));
+		// Only directories.
+		$files = file::scandir($path, false, true);
+		$this->assertSame(true, in_array("{$path}/assets", $files, true));
+		$this->assertSame(false, in_array("{$path}/assets/pi.svg", $files, true));
+		$this->assertSame(false, in_array("{$path}/test-file.php", $files, true));
 	}
 
 	/**
